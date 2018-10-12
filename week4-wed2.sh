cadb342633423c526f05b6409dc88eb98c84f902
cinseire <cinseire.washington@hgs.hiddengeniusproject.org>
2018-10-11 17:14:18 -0700

    first commit

---
+++ week4-wed2.sh
@@ -0,0 +1,13 @@
+cd ~/Desktop
+mkdir week4-wed
+cd week4-wed
+touch index.html
+mkdir css
+mkdir img
+cd css
+touch style1.css
+touch style2.css
+cd ../ img
+curl -o img1.jpg "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/1200px-Lion_waiting_in_Namibia.jpg"
+curl -o img2.jpg "http://www.eatbydate.com/wp-content/uploads/fig.jpg"
+curl -o img3.jpg "https://i.pinimg.com/originals/54/7a/9c/547a9cc6b93e10261f1dd8a8af474e03.jpg"							
